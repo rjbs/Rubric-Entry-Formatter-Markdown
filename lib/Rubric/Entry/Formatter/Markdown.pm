99c80b3538d1eb0077a78d63b6a216aa0d4564f3
Ricardo Signes <rjbs@cpan.org>
2012-02-24 08:13:35 -0500

    dzilify

--- lib/Rubric/Entry/Formatter/Markdown.pm
+++ lib/Rubric/Entry/Formatter/Markdown.pm
@@ -2,15 +2,7 @@ use strict;
 use warnings;
 
 package Rubric::Entry::Formatter::Markdown;
-our $VERSION = '0.554';
-
-=head1 NAME
-
-Rubric::Entry::Formatter::Markdown - format entries with Markdown (duh!)
-
-=head1 VERSION
-
-version 0.554
+# ABSTRACT: format entries with Markdown (duh!)
 
 =head1 DESCRIPTION
 
@@ -26,11 +18,9 @@ cause the formatter to use L<Text::MultiMarkdown> instead of L<Text::Markdown>.
 use Text::Markdown ();
 use Text::MultiMarkdown ();
 
-=head1 METHODS
+=method as_html
 
-=head2 C< as_html >
-
-=head2 C< as_text >
+=method as_text
 
 =cut
 
@@ -49,23 +39,5 @@ sub as_text {
 
   return $arg->{text};
 }
-
-=head1 AUTHOR
-
-Ricardo SIGNES, C<< <rjbs@cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-rubric@rt.cpan.org>, or
-through the web interface at L<http://rt.cpan.org>. I will be notified, and
-then you'll automatically be notified of progress on your bug as I make
-changes.
-
-=head1 COPYRIGHT
-
-Copyright 2005-2006 Ricardo SIGNES.  This program is free software;  you can
-redistribute it and/or modify it under the same terms as Perl itself.
-
-=cut
 
 1;
